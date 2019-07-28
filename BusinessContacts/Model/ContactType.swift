be480c40b6b53ebdb08e3b32cef42d4f6ff67a2b
Nischal Hada <nischal@moroku.com>
2019-07-29 09:05:56 +1000

    Implemented ContactType enum

---
+++ BusinessContacts/Model/ContactType.swift
@@ -0,0 +1,14 @@
+//
+//  ContactType.swift
+//  BusinessContacts
+//
+//  Created by Nischal Hada on 7/26/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+enum ContactType: Int {
+    case all = 0
+    case favourites = 1
+}
